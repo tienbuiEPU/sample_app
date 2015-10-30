ef3af2055739e3ccdc55c9ade88b938df53c642d
tienbuiEPU <tomzz291192@gmail.com>
2015-10-30 22:41:54 +0700

    Finish log-in-log-out

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
-  get 'users/new'
+  # get 'sessions/new'
+
+  # get 'users/new'
 
   # get 'static_pages/...'
 
@@ -13,6 +15,12 @@ Rails.application.routes.draw do
   get 'contact' => 'static_pages#contact'
 
   get 'signup' => 'users#new'
+
+  get 'login' => 'sessions#new'
+
+  post 'login' => 'sessions#create'
+
+  delete 'logout' => 'sessions#destroy'
   
   resources :users
 
